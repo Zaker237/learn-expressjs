9d6de21aeb4dbf8f7e74c72dadee38f7cab9e38a
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2023-12-06 17:48:40 +0100

    resources defaut export

---
+++ src/resources/index.ts
@@ -0,0 +1,5 @@
+export {CardResource} from "./cardResource";
+export {CommentResource} from "./CommentResource";
+export {ProjectResource} from "./ProjectResource";
+export {StepResource} from "./StepResource";
+export {UserResource} from "./UserResource";
