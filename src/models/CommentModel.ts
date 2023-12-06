6e7c65b2c256df9bd8c109015fe5bc495ab64d63
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2023-12-06 17:44:00 +0100

    typos

---
+++ src/models/CommentModel.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, model, Types } from "mongoose";
+
+export interface IComment {
+    createdBy: Types.ObjectId;
+    belongTo: Types.ObjectId,
+    text: string;
+}
+
+const CommentSchema = new Schema<IComment> ({
+    createdBy:{type:Schema.Types.ObjectId, ref:"User", required:true},
+    belongTo:{type:Schema.Types.ObjectId, ref:"Card", required:true},
+    text: {type: String, required: true }
+},
+{ timestamps: true }
+);
+
+export const Comment = model<IComment>("Comment", CommentSchema);
