d9f1753b5cfe1903f7e21bfc1fd307abd9fb4a05
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2023-12-08 16:35:33 +0100

    Project Service

---
+++ src/services/ProjectService.ts
@@ -0,0 +1,123 @@
+import { IProject, Project } from "../models/ProjectModel";
+import { ProjectResource } from "../resources/";
+import mongoose, { Types } from "mongoose";
+// import { dateToString, stringToDate } from "./ServiceHelper";
+// import { ExceptionHandler } from "winston";
+import { User } from "../models/UserModel";
+// import { Card } from "../models/CardModel";
+
+
+export default class ProjectService {
+    public static async getAllProjects(): Promise<ProjectResource[]> {
+        const projects = await Project.find().exec();
+        const projectResponse: ProjectResource[] = [];
+        for (const project of projects) {
+            const resource = this.getProjectAsProjectResource(project);
+            projectResponse.push(resource);
+        }
+        return projectResponse;
+    }
+
+    public static async getProjectsCreatedByUser(userId: string): Promise<ProjectResource[]> {
+        let user = await User.find({ id: new Types.ObjectId(userId) }).exec();
+        if (!user) {
+            throw new Error(`User with ID ${userId} not found`);
+        }
+        const projects = await Project.find({
+            owner: new Types.ObjectId(userId)
+        }).exec();
+
+        const projectResponse: ProjectResource[] = [];
+        for (const project of projects) {
+            const resource = this.getProjectAsProjectResource(project);
+            projectResponse.push(resource)
+        }
+        return projectResponse;
+    }
+
+    public static async getProjectById(id: string): Promise<ProjectResource> {
+        let project = await Project.find({ id: new Types.ObjectId(id) }).exec();
+        if (!project) {
+            throw new Error(`Project with ID ${id} not found`);
+        }
+        return this.getProjectAsProjectResource(project);
+    }
+
+    public static async createProject(project: ProjectResource): Promise<ProjectResource> {
+        let user = await User.find({ id: new Types.ObjectId(project.owner) }).exec();
+        if (!user) {
+            throw new Error(`User with ID ${project.owner} not found`);
+        }
+        const newProject: any = new Project({
+            owner: new Types.ObjectId(project.owner),
+            name: project.name,
+            description: project.description,
+            startAt: project.startAt,
+            endsAt: project.endsAt,
+            public: project.public,
+            closed: project.closed
+        });
+        const existingProject = await Project.findOne({ owner: newProject.owner, name: newProject.name }).exec()
+        if (existingProject) {
+            throw new Error("The Step already exists.");
+        }
+        const savedProject: any = await newProject.save();
+        return this.getProjectAsProjectResource(savedProject);
+    }
+
+    public static async updateProject(project: ProjectResource): Promise<ProjectResource> {
+        let user = await User.find({ id: new Types.ObjectId(project.owner) }).exec();
+        const existingProject = await Project.findById(project.id).exec();
+        if (!existingProject) {
+            throw new Error("The Project does not exist");
+        }
+        if (!user) {
+            throw new Error(`User with ID ${project.owner} not found`);
+        }
+        const updatedProject: any = await Project.findByIdAndUpdate(
+            project.id,
+            {
+                name: project.name,
+                description: project.description,
+                startAt: project.startAt,
+                endsAt: project.endsAt,
+                public: project.public,
+                closed: project.closed,
+                updatedAt: new Date()
+            },
+            { new: true, runValidators: true }
+        ).exec();
+        if (!updatedProject) {
+            throw new Error("An Error occurs when updating");
+        }
+        return this.getProjectAsProjectResource(updatedProject);
+    }
+
+    public static async deleteProject(id: string): Promise<void> {
+        try {
+            const project = await Project.findByIdAndDelete(id).exec();
+            if (!project) {
+                throw new Error(`Project with ID ${id} not found`);
+            }
+            // delete all card in the step
+            //await Card.deleteMany({ inStep: step..toString() }).exec();
+        } catch (error) {
+            throw new Error("Project could not be deleted.")
+        }
+    }
+
+    public static getProjectAsProjectResource(data: any): ProjectResource {
+        const resource: ProjectResource = {
+            id: data.id,
+            owner: data.createdBy.toString(),
+            name: data.name,
+            description: data.description,
+            startAt: data.startAt,
+            endsAt: data.endsAt,
+            public: data.public,
+            closed: data.closed,
+            updatedAt: data.updatedAt
+        }
+        return resource;
+    }
+}
