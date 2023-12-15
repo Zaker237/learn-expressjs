e901eb5528fae3a40c3173e4129147de85112297
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2023-12-15 21:57:52 +0100

    StepService test

--- tests/services/StepService.test.ts
+++ tests/services/StepService.test.ts
@@ -9,13 +9,49 @@ const user1 = {
     email: "test1@email.test", googleId: "googleId"
 };
 
-test('should create a new Step', async () => {
-    const user = await UserService.createUser(user1);
+
+test('should not create a new Step', async () => {
     const step1: StepResource = {
-        createdBy: user.id!,
+        createdBy: "this-string-is-not-an-id",
         name: "step1",
         description: "description"
     };
-    const result = await StepService.createStep(step1);
-    expect(result.name).toEqual(step1.name);
+    expect(async () => await StepService.createStep(step1)).rejects;
+});
+
+
+test('should create a new Step', async () => {
+    const user = await UserService.createUser(user1);
+    const step2: StepResource = {
+        createdBy: user.id!,
+        name: "step2",
+        description: "description"
+    };
+    const result = await StepService.createStep(step2);
+    expect(result.name).toEqual(step2.name);
+});
+
+
+test('should create a step with same name', async () => {
+    const user = await UserService.createUser(user1);
+    const step3: StepResource = {
+        createdBy: user.id!,
+        name: "step3",
+        description: "description"
+    };
+    const result = await StepService.createStep(step3);
+    expect(async () => await StepService.createStep(step3)).rejects;
+});
+
+
+test('should delete Step', async () => {
+    const user = await UserService.createUser(user1);
+    const step4: StepResource = {
+        createdBy: user.id!,
+        name: "step4",
+        description: "description"
+    };
+    const result = await StepService.createStep(step4);
+    expect(result.name).toEqual(step4.name);
+    expect(async () => await StepService.deleteStep(result.id!)).resolves;
 });
